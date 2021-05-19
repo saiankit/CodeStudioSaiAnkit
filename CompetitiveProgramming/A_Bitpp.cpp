817969233d5648f1143cd1f1e1b13d535912bf24
Sai Ankit <saiankit30@gmail.com>
2021-05-19 10:51:35 +0530

    add A_Bitpp.cpp

--- CompetitiveProgramming/A_Bitpp.cpp
+++ CompetitiveProgramming/A_Bitpp.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-  int x = 0;
-  int t;cin>>t;
-  while(t--)
-  {
-    string str;
-    cin>>str;
-    if(str[1]=='+')
-        x++;
-    else if(str[1]=='-')
-        x--;
-  }
-  cout<<x<<"\n";
+
+int main() {
+    int t; cin>>t;
+    int x = 0;
+    while(t--) {
+        string s;
+        cin>>s;
+        if(s[1]=='+') {
+            x++;
+        } else if(s[1]=='-') {
+            x--;
+        }
+    }
+    cout<<x<<"\n";
 }
